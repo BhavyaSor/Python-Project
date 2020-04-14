2792f1e18bbf1440bf6155ed1db63ddcfb2a0c96
vraj1408 <63162696+vraj1408@users.noreply.github.com>
2020-04-14 22:49:55 +0530

    Update Game_1.py

--- Game_1.py
+++ Game_1.py
@@ -118,7 +118,7 @@ def play():
     
         for i in range(num_of_enemies):
     
-            if enemyY[i] > 450:
+            if enemyY[i] > 420:
                 for j in range(num_of_enemies):
                     enemyY[j] = 2000
                 game_over_text()
